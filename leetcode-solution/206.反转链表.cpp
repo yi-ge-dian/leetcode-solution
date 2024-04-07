b3f472fb6a8bb40cad0cc483dd64514600e0f9db
Wenlong Dong <wenldong666@outlook.com>
2024-04-07 23:35:50 +0800

    [链表]-链表反转
    Signed-off-by: Wenlong Dong <wenldong666@outlook.com>

---
+++ leetcode-solution/206.反转链表.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *reverseList(ListNode *head) {
+    ListNode *pre = nullptr;
+
+    auto cur = head;
+
+    while (cur != nullptr) {
+      auto tmp = cur->next;
+      cur->next = pre;
+      pre = cur;
+      cur = tmp;
+    }
+
+    return pre;
+  }
+};
+// @lc code=end
