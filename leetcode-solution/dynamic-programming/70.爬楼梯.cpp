6aba64b5f4d3a93a69c76b2820179facba4867f8
Wenlong Dong <wenldong666@outlook.com>
2024-06-17 00:28:38 +0800

    [DP] 爬楼梯
    Signed-off-by: Wenlong Dong <wenldong666@outlook.com>

---
+++ leetcode-solution/dynamic-programming/70.爬楼梯.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=cpp
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+#include <vector>
+class Solution {
+public:
+  int climbStairs(int n) {
+    if (n <= 2) {
+      return n;
+    }
+
+    std::vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+  }
+};
+// @lc code=end
