327c0c10d69cf7a5ff99396290aa3081cc6998aa
Wenlong Dong <wenldong666@outlook.com>
2024-06-17 00:12:53 +0800

    [dp]: 斐波那契数
    Signed-off-by: Wenlong Dong <wenldong666@outlook.com>

---
+++ leetcode-solution/dynamic-programming/509.斐波那契数.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=cpp
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+#include <vector>
+class Solution {
+public:
+  int fib(int n) {
+    if (n <= 1) {
+      return n;
+    }
+
+    std::vector<int> dp(n + 1, 0);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+  }
+};
+// @lc code=end
